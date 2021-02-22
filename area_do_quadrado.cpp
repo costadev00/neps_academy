b39b0bfe2ba5fd36af2140437b6df255662cb927
Matheus Costa Monteiro <43221564+costadev00@users.noreply.github.com>
2021-02-22 19:48:41 -0300

    started, nice plataform, but cph does not work here

---
+++ area_do_quadrado.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    int l;
+    cin >> l;
+    printf("%d\n", l * l);
+    return 0;
+}
---
+++ ola_codCad.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define fastio                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define endl "\n"
+
+using namespace std;
+
+int main()
+{
+    fastio;
+    printf("Ola CodCad!\n");
+    return 0;
+}
